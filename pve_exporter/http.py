ee8fe171b4bc095911afc0eb544f549201b83ca9
Lorenz Schori <lo@znerol.ch>
2017-08-22 20:21:30 +0200

    Expose connection errors and duration summary in /metrics

--- pve_exporter/http.py
+++ pve_exporter/http.py
@@ -1,27 +1,30 @@
+import time
 import traceback
 import yaml
-from prometheus_client import CONTENT_TYPE_LATEST, generate_latest
+from prometheus_client import CONTENT_TYPE_LATEST, Summary, Counter, generate_latest
 from .collector import collect_pve
 
 try:
     import urlparse
     from BaseHTTPServer import BaseHTTPRequestHandler
     from BaseHTTPServer import HTTPServer
-    from SocketServer import ForkingMixIn
+    from SocketServer import ThreadingMixIn
 except ImportError:
     # python3 renamed those modules, try new names
     import urllib.parse as urlparse
     from http.server import BaseHTTPRequestHandler
     from http.server import HTTPServer
-    from socketserver import ForkingMixIn
+    from socketserver import ThreadingMixIn
 
 
-class ForkingHTTPServer(ForkingMixIn, HTTPServer):
+class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
   pass
 
 class PveExporterHandler(BaseHTTPRequestHandler):
-  def __init__(self, config_path, *args, **kwargs):
+  def __init__(self, config_path, duration, errors, *args, **kwargs):
     self._config_path = config_path
+    self._duration = duration
+    self._errors = errors
     BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
 
   def do_GET(self):
@@ -38,13 +41,16 @@ class PveExporterHandler(BaseHTTPRequestHandler):
         self.wfile.write(b"Module '{0}' not found in config".format(module))
         return
       try:
+        start = time.time()
         target = params.get('target', ['localhost'])[0]
         output = collect_pve(config[module], target)
         self.send_response(200)
         self.send_header('Content-Type', CONTENT_TYPE_LATEST)
         self.end_headers()
         self.wfile.write(output)
+        self._duration.labels(module).observe(time.time() - start)
       except:
+        self._errors.labels(module).inc()
         self.send_response(500)
         self.end_headers()
         self.wfile.write(traceback.format_exc().encode('utf-8'))
@@ -74,8 +80,19 @@ class PveExporterHandler(BaseHTTPRequestHandler):
       self.send_response(404)
       self.end_headers()
 
-def start_http_server(config_path, port):
-  handler = lambda *args, **kwargs: PveExporterHandler(config_path, *args, **kwargs)
-  server = ForkingHTTPServer(('', port), handler)
-  server.serve_forever()
 
+def start_http_server(config_path, port):
+  duration = Summary(
+    'pve_collection_duration_seconds',
+    'Duration of collections by the PVE exporter',
+    ['module'],
+  )
+  errors = Counter(
+    'pve_request_errors_total',
+    'Errors in requests to PVE exporter',
+    ['module'],
+  )
+
+  handler = lambda *args, **kwargs: PveExporterHandler(config_path, duration, errors, *args, **kwargs)
+  server = ThreadingHTTPServer(('', port), handler)
+  server.serve_forever()
