d0404d3432295146e93cac3fb6c4ae6a3c745fd2
znerol <lo+github@znerol.ch>
2017-10-11 22:14:39 +0200

    Convert to PEP-8 coding standards (#5)

--- pve_exporter/collector.py
+++ pve_exporter/collector.py
@@ -1,3 +1,8 @@
+"""
+Prometheus collecters for Proxmox VE cluster.
+"""
+# pylint: disable=too-few-public-methods
+
 import itertools
 from proxmoxer import ProxmoxAPI
 
@@ -5,8 +10,8 @@ from prometheus_client import CollectorRegistry, generate_latest
 from prometheus_client.core import GaugeMetricFamily
 
 class StatusCollector(object):
-  """
-  Collects Proxmox VE Node/VM/CT-Status
+    """
+    Collects Proxmox VE Node/VM/CT-Status
 
     # HELP pve_up Node/VM/CT-Status is online/running
     # TYPE pve_up gauge
@@ -14,239 +19,239 @@ class StatusCollector(object):
     pve_up{id="cluster/pvec"} 1.0
     pve_up{id="lxc/101"} 1.0
     pve_up{id="qemu/102"} 1.0
-  """
+    """
 
-  def __init__(self, pve):
-    self._pve = pve
+    def __init__(self, pve):
+        self._pve = pve
 
-  def collect(self):
-    status_metrics = GaugeMetricFamily(
-      'pve_up',
-      'Node/VM/CT-Status is online/running',
-      labels=['id'])
+    def collect(self): # pylint: disable=missing-docstring
+        status_metrics = GaugeMetricFamily(
+            'pve_up',
+            'Node/VM/CT-Status is online/running',
+            labels=['id'])
 
-    for entry in self._pve.cluster.status.get():
-      if entry['type'] == 'node':
-        label_values = [entry['id']]
-        status_metrics.add_metric(label_values, entry['online'])
-      elif entry['type'] == 'cluster':
-        label_values = ['cluster/{:s}'.format(entry['name'])]
-        status_metrics.add_metric(label_values, entry['quorate'])
-      else:
-        raise ValueError('Got unexpected status entry type {:s}'.format(entry['type']))
+        for entry in self._pve.cluster.status.get():
+            if entry['type'] == 'node':
+                label_values = [entry['id']]
+                status_metrics.add_metric(label_values, entry['online'])
+            elif entry['type'] == 'cluster':
+                label_values = ['cluster/{:s}'.format(entry['name'])]
+                status_metrics.add_metric(label_values, entry['quorate'])
+            else:
+                raise ValueError('Got unexpected status entry type {:s}'.format(entry['type']))
 
-    for resource in self._pve.cluster.resources.get(type='vm'):
-      label_values = [resource['id']]
-      status_metrics.add_metric(label_values, resource['status'] == 'running')
+        for resource in self._pve.cluster.resources.get(type='vm'):
+            label_values = [resource['id']]
+            status_metrics.add_metric(label_values, resource['status'] == 'running')
 
-    yield status_metrics
+        yield status_metrics
 
 class VersionCollector(object):
-  """
-  Collects Proxmox VE build information. E.g.:
+    """
+    Collects Proxmox VE build information. E.g.:
 
     # HELP pve_version_info Proxmox VE version info
     # TYPE pve_version_info gauge
     pve_version_info{release="15",repoid="7599e35a",version="4.4"} 1.0
+    """
 
-  """
+    def __init__(self, pve):
+        self._pve = pve
 
-  def __init__(self, pve):
-    self._pve = pve
+    def collect(self): # pylint: disable=missing-docstring
+        version = self._pve.version.get()
 
-  def collect(self):
-    version = self._pve.version.get()
+        del version['keyboard']
 
-    del version['keyboard']
+        labels, label_values = zip(*version.items())
+        metric = GaugeMetricFamily(
+            'pve_version_info',
+            'Proxmox VE version info',
+            labels=labels
+        )
+        metric.add_metric(label_values, 1)
 
-    labels, label_values = zip(*version.items())
-    metric = GaugeMetricFamily(
-        'pve_version_info',
-        'Proxmox VE version info',
-        labels=labels
-    )
-    metric.add_metric(label_values, 1)
-
-    yield metric
+        yield metric
 
 class ClusterNodeCollector(object):
-  """
-  Collects Proxmox VE cluster node information. E.g.:
+    """
+    Collects Proxmox VE cluster node information. E.g.:
 
     # HELP pve_node_info Node info
     # TYPE pve_node_info gauge
-    pve_node_info{id="node/proxmox-host",ip="10.20.30.40",level="c",local="1",name="proxmox-host",nodeid="0"} 1.0
-  """
+    pve_node_info{id="node/proxmox-host", ip="10.20.30.40", level="c",
+        local="1", name="proxmox-host", nodeid="0"} 1.0
+    """
 
-  def __init__(self, pve):
-    self._pve = pve
+    def __init__(self, pve):
+        self._pve = pve
 
-  def collect(self):
-    nodes = [entry for entry in self._pve.cluster.status.get() if entry['type'] == 'node']
+    def collect(self): # pylint: disable=missing-docstring
+        nodes = [entry for entry in self._pve.cluster.status.get() if entry['type'] == 'node']
 
-    if len(nodes):
-      # Remove superflous keys.
-      for node in nodes:
-        del node['type']
-        del node['online']
+        if nodes:
+            # Remove superflous keys.
+            for node in nodes:
+                del node['type']
+                del node['online']
 
-      # Yield remaining data.
-      labels = nodes[0].keys()
-      info_metrics = GaugeMetricFamily(
-            'pve_node_info',
-            'Node info',
-            labels=labels)
+            # Yield remaining data.
+            labels = nodes[0].keys()
+            info_metrics = GaugeMetricFamily(
+                'pve_node_info',
+                'Node info',
+                labels=labels)
 
-      for node in nodes:
-        label_values = [str(node[key]) for key in labels]
-        info_metrics.add_metric(label_values, 1)
+            for node in nodes:
+                label_values = [str(node[key]) for key in labels]
+                info_metrics.add_metric(label_values, 1)
 
-      yield info_metrics
+            yield info_metrics
 
 class ClusterInfoCollector(object):
-  """
-  Collects Proxmox VE cluster information. E.g.:
+    """
+    Collects Proxmox VE cluster information. E.g.:
 
     # HELP pve_cluster_info Cluster info
     # TYPE pve_cluster_info gauge
     pve_cluster_info{id="cluster/pvec",nodes="2",quorate="1",version="2"} 1.0
-  """
+    """
 
-  def __init__(self, pve):
-    self._pve = pve
+    def __init__(self, pve):
+        self._pve = pve
 
-  def collect(self):
-    clusters = [entry for entry in self._pve.cluster.status.get() if entry['type'] == 'cluster']
+    def collect(self): # pylint: disable=missing-docstring
+        clusters = [entry for entry in self._pve.cluster.status.get() if entry['type'] == 'cluster']
 
-    if len(clusters):
-      # Remove superflous keys.
-      for cluster in clusters:
-        del cluster['type']
+        if clusters:
+            # Remove superflous keys.
+            for cluster in clusters:
+                del cluster['type']
 
-      # Add cluster-prefix to id.
-      for cluster in clusters:
-        cluster['id'] = 'cluster/{:s}'.format(cluster['name'])
-        del cluster['name']
+            # Add cluster-prefix to id.
+            for cluster in clusters:
+                cluster['id'] = 'cluster/{:s}'.format(cluster['name'])
+                del cluster['name']
 
-      # Yield remaining data.
-      labels = clusters[0].keys()
-      info_metrics = GaugeMetricFamily(
-            'pve_cluster_info',
-            'Cluster info',
-            labels=labels)
+            # Yield remaining data.
+            labels = clusters[0].keys()
+            info_metrics = GaugeMetricFamily(
+                'pve_cluster_info',
+                'Cluster info',
+                labels=labels)
 
-      for cluster in clusters:
-        label_values = [str(cluster[key]) for key in labels]
-        info_metrics.add_metric(label_values, 1)
+            for cluster in clusters:
+                label_values = [str(cluster[key]) for key in labels]
+                info_metrics.add_metric(label_values, 1)
 
-      yield info_metrics
+            yield info_metrics
 
 class ClusterResourcesCollector(object):
-  """
-  Collects Proxmox VE cluster resources information, i.e. memory, storage, cpu
-  usage for cluster nodes and guests.
-  """
+    """
+    Collects Proxmox VE cluster resources information, i.e. memory, storage, cpu
+    usage for cluster nodes and guests.
+    """
 
-  def __init__(self, pve):
-    self._pve = pve
+    def __init__(self, pve):
+        self._pve = pve
 
-  def collect(self):
-    metrics = {
-        'maxdisk': GaugeMetricFamily(
-          'pve_disk_size_bytes',
-          'Size of storage device',
-          labels=['id']),
-        'disk': GaugeMetricFamily(
-          'pve_disk_usage_bytes',
-          'Disk usage in bytes',
-          labels=['id']),
-        'maxmem': GaugeMetricFamily(
-          'pve_memory_size_bytes',
-          'Size of memory',
-          labels=['id']),
-        'mem': GaugeMetricFamily(
-          'pve_memory_usage_bytes',
-          'Memory usage in bytes',
-          labels=['id']),
-        'netout': GaugeMetricFamily(
-          'pve_network_transmit_bytes',
-          'Number of bytes transmitted over the network',
-          labels=['id']),
-        'netin': GaugeMetricFamily(
-          'pve_network_receive_bytes',
-          'Number of bytes received over the network',
-          labels=['id']),
-        'diskwrite': GaugeMetricFamily(
-          'pve_disk_write_bytes',
-          'Number of bytes written to storage',
-          labels=['id']),
-        'diskread': GaugeMetricFamily(
-          'pve_disk_read_bytes',
-          'Number of bytes read from storage',
-          labels=['id']),
-        'cpu': GaugeMetricFamily(
-          'pve_cpu_usage_ratio',
-          'CPU usage (value between 0.0 and pve_cpu_usage_limit)',
-          labels=['id']),
-        'maxcpu': GaugeMetricFamily(
-          'pve_cpu_usage_limit',
-          'Maximum allowed CPU usage',
-          labels=['id']),
-        'uptime': GaugeMetricFamily(
-          'pve_uptime_seconds',
-          'Number of seconds since the last boot',
-          labels=['id']),
+    def collect(self): # pylint: disable=missing-docstring
+        metrics = {
+            'maxdisk': GaugeMetricFamily(
+                'pve_disk_size_bytes',
+                'Size of storage device',
+                labels=['id']),
+            'disk': GaugeMetricFamily(
+                'pve_disk_usage_bytes',
+                'Disk usage in bytes',
+                labels=['id']),
+            'maxmem': GaugeMetricFamily(
+                'pve_memory_size_bytes',
+                'Size of memory',
+                labels=['id']),
+            'mem': GaugeMetricFamily(
+                'pve_memory_usage_bytes',
+                'Memory usage in bytes',
+                labels=['id']),
+            'netout': GaugeMetricFamily(
+                'pve_network_transmit_bytes',
+                'Number of bytes transmitted over the network',
+                labels=['id']),
+            'netin': GaugeMetricFamily(
+                'pve_network_receive_bytes',
+                'Number of bytes received over the network',
+                labels=['id']),
+            'diskwrite': GaugeMetricFamily(
+                'pve_disk_write_bytes',
+                'Number of bytes written to storage',
+                labels=['id']),
+            'diskread': GaugeMetricFamily(
+                'pve_disk_read_bytes',
+                'Number of bytes read from storage',
+                labels=['id']),
+            'cpu': GaugeMetricFamily(
+                'pve_cpu_usage_ratio',
+                'CPU usage (value between 0.0 and pve_cpu_usage_limit)',
+                labels=['id']),
+            'maxcpu': GaugeMetricFamily(
+                'pve_cpu_usage_limit',
+                'Maximum allowed CPU usage',
+                labels=['id']),
+            'uptime': GaugeMetricFamily(
+                'pve_uptime_seconds',
+                'Number of seconds since the last boot',
+                labels=['id']),
         }
 
-    info_metrics = {
-        'guest': GaugeMetricFamily(
-          'pve_guest_info',
-          'VM/CT info',
-          labels=['id', 'node', 'name', 'type']),
-        'storage': GaugeMetricFamily(
-          'pve_storage_info',
-          'Storage info',
-          labels=['id', 'node', 'storage']),
+        info_metrics = {
+            'guest': GaugeMetricFamily(
+                'pve_guest_info',
+                'VM/CT info',
+                labels=['id', 'node', 'name', 'type']),
+            'storage': GaugeMetricFamily(
+                'pve_storage_info',
+                'Storage info',
+                labels=['id', 'node', 'storage']),
         }
 
-    info_lookup = {
-        'lxc': {
-          'labels': ['id', 'node', 'name', 'type'],
-          'gauge': info_metrics['guest'],
-          },
-        'qemu': {
-          'labels': ['id', 'node', 'name', 'type'],
-          'gauge': info_metrics['guest'],
-          },
-        'storage': {
-          'labels': ['id', 'node', 'storage'],
-          'gauge': info_metrics['storage'],
-          },
+        info_lookup = {
+            'lxc': {
+                'labels': ['id', 'node', 'name', 'type'],
+                'gauge': info_metrics['guest'],
+            },
+            'qemu': {
+                'labels': ['id', 'node', 'name', 'type'],
+                'gauge': info_metrics['guest'],
+            },
+            'storage': {
+                'labels': ['id', 'node', 'storage'],
+                'gauge': info_metrics['storage'],
+            },
         }
 
-    for resource in self._pve.cluster.resources.get():
-      restype = resource['type']
+        for resource in self._pve.cluster.resources.get():
+            restype = resource['type']
 
-      if restype in info_lookup:
-        label_values = [resource[key] for key in info_lookup[restype]['labels']]
-        info_lookup[restype]['gauge'].add_metric(label_values, 1)
+            if restype in info_lookup:
+                label_values = [resource[key] for key in info_lookup[restype]['labels']]
+                info_lookup[restype]['gauge'].add_metric(label_values, 1)
 
-      label_values = [resource['id']]
-      for key, metric_value in resource.items():
-        if key in metrics:
-          metrics[key].add_metric(label_values, metric_value)
+            label_values = [resource['id']]
+            for key, metric_value in resource.items():
+                if key in metrics:
+                    metrics[key].add_metric(label_values, metric_value)
 
-    return itertools.chain(metrics.values(), info_metrics.values())
+        return itertools.chain(metrics.values(), info_metrics.values())
 
 def collect_pve(config, host):
-  """Scrape a host and return prometheus text format for it"""
+    """Scrape a host and return prometheus text format for it"""
 
-  pve = ProxmoxAPI(host, **config)
+    pve = ProxmoxAPI(host, **config)
 
-  registry = CollectorRegistry()
-  registry.register(StatusCollector(pve))
-  registry.register(ClusterResourcesCollector(pve))
-  registry.register(ClusterNodeCollector(pve))
-  registry.register(ClusterInfoCollector(pve))
-  registry.register(VersionCollector(pve))
-  return generate_latest(registry)
+    registry = CollectorRegistry()
+    registry.register(StatusCollector(pve))
+    registry.register(ClusterResourcesCollector(pve))
+    registry.register(ClusterNodeCollector(pve))
+    registry.register(ClusterInfoCollector(pve))
+    registry.register(VersionCollector(pve))
+    return generate_latest(registry)
--- pve_exporter/http.py
+++ pve_exporter/http.py
@@ -1,3 +1,6 @@
+"""
+HTTP API for Proxmox VE prometheus collector.
+"""
 import time
 import traceback
 import yaml
@@ -18,90 +21,103 @@ except ImportError:
 
 
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
-  pass
+    """
+    Multithreaded HTTP Server.
+    """
+    pass
 
 class PveExporterHandler(BaseHTTPRequestHandler):
-  def __init__(self, config, duration, errors, *args, **kwargs):
-    self._config = config
-    self._duration = duration
-    self._errors = errors
-    BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
+    """
+    Proxmox VE prometheus collector HTTP handler.
+    """
+    def __init__(self, config, duration, errors, *args, **kwargs):
+        self._config = config
+        self._duration = duration
+        self._errors = errors
+        BaseHTTPRequestHandler.__init__(self, *args, **kwargs)
 
-  def do_GET(self):
-    url = urlparse.urlparse(self.path)
+    def do_GET(self): # pylint: disable=invalid-name
+        """
+        HTTP handler for GET requests.
+        """
+        url = urlparse.urlparse(self.path)
 
-    if url.path == '/pve':
-      params = urlparse.parse_qs(url.query)
-      module = params.get("module", ["default"])[0]
-      if module not in self._config:
-        self.send_response(400)
-        self.end_headers()
-        self.wfile.write(b"Module '{0}' not found in config".format(module))
-        return
-      try:
-        start = time.time()
-        target = params.get('target', ['localhost'])[0]
-        output = collect_pve(self._config[module], target)
-        self.send_response(200)
-        self.send_header('Content-Type', CONTENT_TYPE_LATEST)
-        self.end_headers()
-        self.wfile.write(output)
-        self._duration.labels(module).observe(time.time() - start)
-      except:
-        self._errors.labels(module).inc()
-        self.send_response(500)
-        self.end_headers()
-        self.wfile.write(traceback.format_exc().encode('utf-8'))
-    elif url.path == '/metrics':
-      try:
-        output = generate_latest()
-        self.send_response(200)
-        self.send_header('Content-Type', CONTENT_TYPE_LATEST)
-        self.end_headers()
-        self.wfile.write(output)
-      except:
-        self.send_response(500)
-        self.end_headers()
-        self.wfile.write(traceback.format_exc().encode('utf-8'))
+        if url.path == '/pve':
+            params = urlparse.parse_qs(url.query)
+            module = params.get("module", ["default"])[0]
+            if module not in self._config:
+                self.send_response(400)
+                self.end_headers()
+                self.wfile.write(b"Module '{0}' not found in config".format(module))
+                return
+            try:
+                start = time.time()
+                target = params.get('target', ['localhost'])[0]
+                output = collect_pve(self._config[module], target)
+                self.send_response(200)
+                self.send_header('Content-Type', CONTENT_TYPE_LATEST)
+                self.end_headers()
+                self.wfile.write(output)
+                self._duration.labels(module).observe(time.time() - start)
+            except Exception: # pylint: disable=broad-except
+                self._errors.labels(module).inc()
+                self.send_response(500)
+                self.end_headers()
+                self.wfile.write(traceback.format_exc().encode('utf-8'))
+        elif url.path == '/metrics':
+            try:
+                output = generate_latest()
+                self.send_response(200)
+                self.send_header('Content-Type', CONTENT_TYPE_LATEST)
+                self.end_headers()
+                self.wfile.write(output)
+            except Exception: # pylint: disable=broad-except
+                self.send_response(500)
+                self.end_headers()
+                self.wfile.write(traceback.format_exc().encode('utf-8'))
 
-    elif url.path == '/':
-      self.send_response(200)
-      self.end_headers()
-      self.wfile.write(b"""<html>
-      <head><title>Proxmox VE Exporter</title></head>
-      <body>
-      <h1>Proxmox VE Exporter</h1>
-      <p>Visit <code>/pve?target=1.2.3.4</code> to use.</p>
-      </body>
-      </html>""")
-    else:
-      self.send_response(404)
-      self.end_headers()
+        elif url.path == '/':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"""<html>
+                             <head><title>Proxmox VE Exporter</title></head>
+                             <body>
+                             <h1>Proxmox VE Exporter</h1>
+                             <p>Visit <code>/pve?target=1.2.3.4</code> to use.</p>
+                             </body>
+                             </html>""")
+        else:
+            self.send_response(404)
+            self.end_headers()
 
 
 def start_http_server(config_path, port):
-  duration = Summary(
-    'pve_collection_duration_seconds',
-    'Duration of collections by the PVE exporter',
-    ['module'],
-  )
-  errors = Counter(
-    'pve_request_errors_total',
-    'Errors in requests to PVE exporter',
-    ['module'],
-  )
+    """
+    Start a HTTP API server for Proxmox VE prometheus collector.
+    """
 
-  # Load configuration.
-  with open(config_path) as f:
-    config = yaml.safe_load(f)
+    duration = Summary(
+        'pve_collection_duration_seconds',
+        'Duration of collections by the PVE exporter',
+        ['module'],
+    )
+    errors = Counter(
+        'pve_request_errors_total',
+        'Errors in requests to PVE exporter',
+        ['module'],
+    )
 
-  # Initialize metrics.
-  for module in config.keys():
-    # pylint: disable=no-member
-    errors.labels(module)
-    # pylint: disable=no-member
-    duration.labels(module)
+    # Load configuration.
+    with open(config_path) as handle:
+        config = yaml.safe_load(handle)
 
-  handler = lambda *args, **kwargs: PveExporterHandler(config, duration, errors, *args, **kwargs)
-  server = ThreadingHTTPServer(('', port), handler)
-  server.serve_forever()
+    # Initialize metrics.
+    for module in config.keys():
+        # pylint: disable=no-member
+        errors.labels(module)
+        # pylint: disable=no-member
+        duration.labels(module)
+
+    handler = lambda *args, **kwargs: PveExporterHandler(config, duration, errors, *args, **kwargs)
+    server = ThreadingHTTPServer(('', port), handler)
+    server.serve_forever()
