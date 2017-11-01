8665d87ca9006353f14aa7546c91f58a9ff362d4
Lorenz Schori <lo@znerol.ch>
2017-11-01 09:11:52 +0100

    Generate long_description from README.txt, fix package name bump version

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
+import codecs
 import os
 from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name = "pve_exporter",
-    version = "0.0.1",
+    name = "prometheus-pve-exporter",
+    version = "1.0.0",
     author = "Lorenz Schori",
     author_email = "lo@znerol.ch",
     description = ("Proxmox VE exporter for the Prometheus monitoring system."),
-    long_description = ("See https://github.com/znerol/prometheus-pve-exporter/blob/master/README.md for documentation."),
+    long_description = codecs.open('README.rst', encoding='utf-8').read(),
     license = "Apache Software License 2.0",
     keywords = "prometheus exporter network monitoring proxmox",
     url = "https://github.com/znerol/prometheus-pve-exporter",
