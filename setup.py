9534587e4aed12558e280f32dc79c9b34c50b480
Lorenz Schori <lo@znerol.ch>
2022-03-06 12:48:29 +0100

    Update changelog, bump release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="prometheus-pve-exporter",
-    version="2.2.2",
+    version="2.2.3",
     author="Lorenz Schori",
     author_email="lo@znerol.ch",
     description=("Proxmox VE exporter for the Prometheus monitoring system."),
