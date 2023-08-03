87a6d36fecad01e1de27718de109d29406b2cbaa
znerol <lo+github@znerol.ch>
2023-08-03 07:55:08 +0200

    Remove fallback for BooleanOptionalAction (required for python<3.9

--- src/pve_exporter/cli.py
+++ src/pve_exporter/cli.py
@@ -11,52 +11,7 @@ from pve_exporter.config import config_from_yaml
 from pve_exporter.config import config_from_env
 from pve_exporter.collector import CollectorsOptions
 
-try:
-    from argparse import BooleanOptionalAction
-except ImportError:
-    from argparse import Action
-    # https://github.com/python/cpython/blob/master/Lib/argparse.py#L856
-    # pylint: disable=all
-    class BooleanOptionalAction(Action):
-        def __init__(self,
-                     option_strings,
-                     dest,
-                     default=None,
-                     type=None,
-                     choices=None,
-                     required=False,
-                     help=None,
-                     metavar=None):
-
-            _option_strings = []
-            for option_string in option_strings:
-                _option_strings.append(option_string)
-
-                if option_string.startswith('--'):
-                    option_string = '--no-' + option_string[2:]
-                    _option_strings.append(option_string)
-
-            if help is not None and default is not None:
-                help += f" (default: {default})"
-
-            super().__init__(
-                option_strings=_option_strings,
-                dest=dest,
-                nargs=0,
-                default=default,
-                type=type,
-                choices=choices,
-                required=required,
-                help=help,
-                metavar=metavar)
-
-        def __call__(self, parser, namespace, values, option_string=None):
-            if option_string in self.option_strings:
-                setattr(namespace, self.dest, not option_string.startswith('--no-'))
-
-        def format_usage(self):
-            return ' | '.join(self.option_strings)
-
+from argparse import BooleanOptionalAction
 
 def main():
     """
