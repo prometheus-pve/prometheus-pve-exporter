3a17ba074d5da3cb275b7b18cf8cab55005dca24
znerol <lo+github@znerol.ch>
2024-01-04 22:20:59 +0100

    Use pyproject.toml, pip-compile and venv to build container image

--- setup.py
+++ setup.py
@@ -1,38 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
-setup(
-    name="prometheus-pve-exporter",
-    version="3.1.0",
-    author="Lorenz Schori",
-    author_email="lo@znerol.ch",
-    description=("Proxmox VE exporter for the Prometheus monitoring system."),
-    long_description=open('README.rst').read(),
-    license="Apache Software License 2.0",
-    keywords="prometheus exporter network monitoring proxmox",
-    url="https://github.com/prometheus-pve/prometheus-pve-exporter",
-    package_dir={"": "src"},
-    packages=find_packages('src'),
-    entry_points={
-        'console_scripts': [
-            'pve_exporter=pve_exporter.cli:main',
-        ],
-    },
-    python_requires=">=3.9",
-    install_requires=[
-        "prometheus_client>=0.0.11",
-        "proxmoxer",
-        "pyyaml",
-        "requests",
-        'Werkzeug',
-        'gunicorn',
-    ],
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: System Administrators",
-        "Topic :: System :: Monitoring",
-        "Topic :: System :: Networking :: Monitoring",
-        "License :: OSI Approved :: Apache Software License",
-    ],
-)
+setup()
